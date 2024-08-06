a65fa3ccc8cb4c90f1ac9b32361ef65abe8999b7
Omar Elmanzalawy <90012228+OmarElmanzalawy@users.noreply.github.com>
2024-08-06 17:55:53 +0300

    added route names into a class

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:ulearn_riverpod/common/routes/route_names.dart';
 import 'package:ulearn_riverpod/common/utils/app_styles.dart';
 import 'package:ulearn_riverpod/firebase_options.dart';
 import 'package:ulearn_riverpod/global.dart';
+import 'package:ulearn_riverpod/pages/home/home.dart';
 import 'package:ulearn_riverpod/pages/sign_in/sign_in.dart';
 import 'package:ulearn_riverpod/pages/sign_up/sign_up.dart';
 import 'package:ulearn_riverpod/pages/welcome/welcome.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
-  Global.init();
+  await Global.init();
   runApp(const ProviderScope(child: MainApp()));
 }
 
@@ -20,11 +23,13 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.appThemeData,
-      initialRoute: '/',
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? RouteNames.WELCOME : RouteNames.HOME,
       routes: {
-        '/': (context) => WelcomeScreen(),
-        '/signin': (context) => const SignIn(),
-        '/signup': (context) => const SignUp(),
+        RouteNames.WELCOME: (context) => WelcomeScreen(),
+        RouteNames.SIGN_IN: (context) => const SignIn(),
+        RouteNames.SIGN_UP: (context) => const SignUp(),
+       RouteNames.HOME: (context) => const Home(),
       },
     );
   }
